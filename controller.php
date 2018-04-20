9dd9f1acbbaa227c71bff5a7a8d3146839d1cf62
Laura Shoemake <laura@dhcp-10-142-159-181.uawifi.arizona.edu>
2018-04-20 13:44:08 -0700

    init commit

---
+++ controller.php
@@ -0,0 +1,42 @@
+<?php
+//get ingredients
+$call = "";
+if(isset($_GET['call']))
+{
+    $call = $_GET['call'];
+}
+
+//get ingredients
+if($call == 'ingredients')
+{
+    echo 'hi';
+}
+
+//get recipes
+if($call == 'recipes')
+{
+    echo 'bye';
+}
+
+//login
+else if(isset($_POST['ID'])  && isset($_POST['password']))
+{
+}
+
+//register
+else if(isset($_POST['regID'])  && isset($_POST['regPassword']) && isset($_POST['firstName']) && isset($_POST['lastName']))
+{
+    
+}
+
+//add ingredient
+else if(isset($_POST['ingName'])  && isset($_POST['ingCost']) && isset($_POST['ingUnit']) && isset($_POST['ingDescription']))
+{
+    
+}
+
+//add recipe
+else if(isset($_POST['recName'])  && isset($_POST['recIng']) &&  isset($_POST['recDescription']))
+{
+
+}
